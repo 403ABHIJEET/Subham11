36ed57dee9b4cbbb65833206c7dabad9babfc378
403ABHIJEET <abhijeet8745@gmail.com>
2024-12-08 11:37:39 +0530

    vercel issue

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 
